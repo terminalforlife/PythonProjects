cf4a9733e353257460c94346cde303fe536d9d14
terminalforlife <terminalforlife@yahoo.com>
2023-05-25 22:54:56 +0100

    Add gtkdemo.py

---
+++ source/gtkdemo.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3.6
+
+#------------------------------------------------------------------------------
+# Project Name      - PythonProjects/source/gtkdemo.py
+# Started On        - Thu 25 May 00:00:01 BST 2023
+# Last Change       - Thu 25 May 22:54:28 BST 2023
+# Author E-Mail     - terminalforlife@yahoo.com
+# Author GitHub     - https://github.com/terminalforlife
+#------------------------------------------------------------------------------
+# Demonstrating using a basic Gtk GUI to print out the output entered into an
+# Entry widget via a Button widget. Even with the class, this is easier than in
+# Perl, especially as Gtk is well documented in Python!
+#------------------------------------------------------------------------------
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class Root(Gtk.Window):
+	# The constructor.
+	def __init__(self):
+		# The Gtk.Window constructor, to set the title.
+		Gtk.Window.__init__(self, title='Test')
+		self.set_default_geometry(400, 30)
+
+		# Create the box inside the main window.
+		self.box = Gtk.Box()
+		self.add(self.box)
+
+		# Create a button, set up an event, and add to box.
+		self.button = Gtk.Button(label='Click Me')
+		self.button.connect('clicked', self.button_clicked)
+		self.box.pack_start(self.button, True, True, 0)
+
+		self.input = Gtk.Entry()
+		self.box.pack_end(self.input, True, True, 0)
+
+	def button_clicked(self, widget):
+		print(self.input.get_text())
+
+
+window = Root()
+window.connect('delete-event', Gtk.main_quit)
+window.show_all()
+
+Gtk.main()
