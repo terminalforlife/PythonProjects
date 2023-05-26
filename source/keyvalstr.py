8c24299eca3444b925f62ad39140c15726eea53c
terminalforlife <terminalforlife@yahoo.com>
2023-05-26 16:25:42 +0100

    Add comment & remove old increment line

--- source/keyvalstr.py
+++ source/keyvalstr.py
@@ -3,12 +3,18 @@
 #------------------------------------------------------------------------------
 # Project Name      - PythonProjects/source/keyvalstr.py
 # Started On        - Wed 24 May 20:55:59 BST 2023
-# Last Change       - Thu 25 May 22:57:02 BST 2023
+# Last Change       - Fri 26 May 16:25:39 BST 2023
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
 # Iterate over a key=value string character-by-character to fetch both the key
 # and the value, separately. I'm really hating 'camelCase', but, when in Rome.
+#
+# Using Python features, I'd probably write:
+#
+#   key, value = (string.split('='))
+#
+# Although that's not as robust. The below allows for '=' in the value.
 #------------------------------------------------------------------------------
 
 string = 'key=value'
@@ -26,8 +32,6 @@ for index in range(0, len(string)):
 		key = out
 	else:
 		out = out + char
-
-	index += 1
 
 print(key)
 print(value)
