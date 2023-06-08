eb101f94d9933133732f17b851ac33356474b27e
terminalforlife <terminalforlife@yahoo.com>
2023-06-08 16:22:09 +0100

    Correct error messages

--- source/randomword.py
+++ source/randomword.py
@@ -3,7 +3,7 @@
 #------------------------------------------------------------------------------
 # Project Name      - PythonProjects/source/randomword.py
 # Started On        - Thu 25 May 12:51:27 BST 2023
-# Last Change       - Thu 25 May 22:58:43 BST 2023
+# Last Change       - Thu  8 Jun 16:22:07 BST 2023
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
@@ -32,9 +32,9 @@ try:
 			if line[-2:-1] != "'" and line[0:1] != line[0:1].upper():
 				words.append(line)
 except FileNotFoundError:
-	err(1, "Err: File '{}' not found.".format(file))
+	err(1, "File '{}' not found.".format(file))
 except PermissionError:
-	err(1, "Err: File '{}' inaccessible.".format(file))
+	err(1, "File '{}' inaccessible.".format(file))
 
 # Both approaches work, but the latter is far more appropriate.
 #print(words[randint(0, len(words) - 1)])
