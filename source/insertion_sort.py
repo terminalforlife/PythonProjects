fb0972636cc34c40524e58370ec3e4ef2404bb2a
terminalforlife <terminalforlife@yahoo.com>
2023-06-10 23:59:17 +0100

    Add to comment header (about Timsort)

--- source/insertion_sort.py
+++ source/insertion_sort.py
@@ -3,12 +3,14 @@
 #------------------------------------------------------------------------------
 # Project Name      - PythonProjects/source/insertion_sort.py
 # Started On        - Wed 24 May 23:48:07 BST 2023
-# Last Change       - Sat 10 Jun 23:26:12 BST 2023
+# Last Change       - Sat 10 Jun 23:59:15 BST 2023
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
-# Insertion Sort arglorithm in Python. Unfortunately, it seems Python doesn't
-# support the standard `--pos` or `pos--` syntax, which royally sucks.
+# Insertion Sort algorithm in Python. Unfortunately, it seems Python doesn't
+# support the standard `--pos` or `pos--` syntax, which royally sucks. Python
+# uses Timsort, named after Tim Peters who wrote an ingenious mix of merge sort
+# and insertion sort in 2002.
 #------------------------------------------------------------------------------
 
 List = [8, 1, 7, 4, 9, 3, 6, 10, 5, 2]
