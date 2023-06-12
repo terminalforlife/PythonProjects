9b5ba24c8864fe24b83fc6d478bb6f0628fa395c
terminalforlife <terminalforlife@yahoo.com>
2023-06-12 12:30:06 +0100

    Prefer `perf_counter()` method for accuracy
    I know `time.time()` is for the SSE (and possible split second) and
    not designed for benchmarking, but I didn't know the `perf_counter()`
    method existed, so it's definitely worth just using that.

--- source/fibdemo.py
+++ source/fibdemo.py
@@ -3,14 +3,14 @@
 #------------------------------------------------------------------------------
 # Project Name      - PythonProjects/source/fibdemo.py
 # Started On        - Sun  4 Jun 01:33:53 BST 2023
-# Last Change       - Thu  8 Jun 16:13:07 BST 2023
+# Last Change       - Mon 12 Jun 12:29:50 BST 2023
 # Author E-Mail     - terminalforlife@yahoo.com
 # Author GitHub     - https://github.com/terminalforlife
 #------------------------------------------------------------------------------
 # A demonstration of the obligatory Fibonacci Sequence recursive function. This
-# file includes both the simple version and the more advanced version. Python
-# 3.6 refuses to recurse beyond the 999th time, while Python 3.9 won't go past
-# 995! Argh!
+# file includes both the simple version and the more advanced version which
+# uses memoization. Python 3.6 refuses to recurse beyond the 999th time, while
+# Python 3.9 won't go past 995! Argh!
 #
 # This was so much easier than in BASH!
 #
@@ -27,11 +27,11 @@ def Fib(Nr) -> int:
 
 print('Using simple method for #37:\n')
 
-SecsBefore = int(time.time())
+SecsBefore = int(time.perf_counter())
 
 Fib(37)
 
-SecsAfter = int(time.time())
+SecsAfter = int(time.perf_counter())
 print('\nTook {} second(s).\n'.format(SecsAfter - SecsBefore))
 
 Nrs = {0: 0, 1: 1}
@@ -46,9 +46,9 @@ def Fib(Nr) -> int:
 
 print('Using advanced method for #995:\n')
 
-SecsBefore = int(time.time())
+SecsBefore = int(time.perf_counter())
 
 print(Fib(995))
 
-SecsAfter = int(time.time())
+SecsAfter = int(time.perf_counter())
 print('\nTook {} second(s).\n'.format(SecsAfter - SecsBefore))
